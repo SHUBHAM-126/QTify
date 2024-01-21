86ccd73d103fe77929c61c464b7ff52da250c469
SHUBHAM-126 <shubhammhatre126@gmail.com>
2024-01-21 15:21:32 +0530

    Minor fix - Logo

--- qtify-app/src/components/Logo/Logo.jsx
+++ qtify-app/src/components/Logo/Logo.jsx
@@ -3,7 +3,7 @@ import styles from './Logo.module.css'
 import logo from '../../assets/logo.png'
 
 function Logo() {
-    return(<img src={logo} height={34} />)
+    return(<img src={logo} width={67} alt='logo' />)
 }
 
 export default Logo
